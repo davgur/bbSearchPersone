fb49df403d119cc10014dd59ba9708a4c7c42dfa
David <david.gurovich@tibaparking.com>
2017-03-08 15:23:56 +0200

    add web

---
+++ src/main/java/search/IPersoneEntity.java
@@ -0,0 +1,14 @@
+package search;
+
+public interface IPersoneEntity {
+	String getName();
+
+	String getTranslit();
+
+	String getTelephone();
+
+	String getCarNumber();
+
+	PersoneStatusEnum getStatus();
+
+}
